61d4d3269c24ca2b00b3e00561bb226f8f61ebde
ikoba12 <ikoba12@freeuni.edu.ge>
2015-07-06 23:29:57 +0300

    playlist

---
+++ CloudPlayer/app/src/main/java/com/example/user/cloudplayer/adapters/SongAdapter.java
@@ -0,0 +1,55 @@
+package com.example.user.cloudplayer.adapters;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.user.cloudplayer.R;
+import com.example.user.cloudplayer.model.PlayList;
+import com.example.user.cloudplayer.model.Song;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Irakli on 7/6/2015.
+ */
+public class SongAdapter extends BaseAdapter{
+
+    private Activity context;
+    private ArrayList<Song> songs;
+
+    public SongAdapter(Activity context, ArrayList<Song> songs){
+        this.context = context;
+        this.songs = songs;
+    }
+
+    @Override
+    public int getCount() {
+        return songs.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return songs.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(view == null){ // inflates only when it is necessary.it is correct style of writing such as code.
+            view = View.inflate(context, R.layout.activity_playlist_list_view_item,null);
+        }
+
+        TextView songName = (TextView)view.findViewById(R.id.activity_playlist_name);
+
+        view.setMinimumHeight(125);
+
+        return view;
+    }
+}
--- CloudPlayer/app/src/main/java/com/example/user/cloudplayer/ui/PlayListActivity.java
+++ CloudPlayer/app/src/main/java/com/example/user/cloudplayer/ui/PlayListActivity.java
@@ -1,10 +1,76 @@
 package com.example.user.cloudplayer.ui;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.user.cloudplayer.R;
+import com.example.user.cloudplayer.adapters.ProfileActivityAdapter;
+import com.example.user.cloudplayer.adapters.SongAdapter;
+import com.example.user.cloudplayer.model.PlayList;
+import com.example.user.cloudplayer.model.Song;
 import com.example.user.cloudplayer.transport.NetworkEventListener;
+
+import java.util.ArrayList;
 
 
 public class PlayListActivity extends Activity implements NetworkEventListener {
+    private ListView list;
+    private SongAdapter adapter;
+    private Button comment;
+    private Button like;
+    private TextView numLikes;
+    private ArrayList<Song> currentPlayList;
+    private Button addSong;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist);
+        final int playlistId = 1;
+        final Activity a = this;
+        comment = (Button) findViewById(R.id.comment);
+        like = (Button) findViewById(R.id.like_button);
+        addSong = (Button) findViewById(R.id.add_button);
+        numLikes = (TextView) findViewById(R.id.num_likes);
+        list = (ListView) findViewById(R.id.song_list);
+
+        comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(a, CommentsActivity.class);
+                intent.putExtra(a.getResources().getString(R.string.key_playlistID), playlistId);
+                startActivity(intent);
+            }
+        });
+        addSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //if(cloudStorage.getUser()==me){
+        // like.setText("unlike"));
+        //addSong.setVisisibility(visible);
+        //}
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //App.onLikeButtonClicked();
+            }
+        });
+        currentPlayList=new ArrayList<>();
+        for(int i=0;i<20;i++){
+            currentPlayList.add(new Song());
+        }
+        adapter=new SongAdapter(this,currentPlayList);
+        list.setAdapter(adapter);
+
+
+    }
 }
