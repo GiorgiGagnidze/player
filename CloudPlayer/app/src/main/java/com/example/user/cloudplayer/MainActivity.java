b4a09d4a31d3db05bc9cf2925fb6f1ce7a32f42c
GiorgiGagnidze <GiorgiGagnidze@users.noreply.github.com>
2015-07-08 18:41:11 +0400

    comit

--- CloudPlayer/app/src/main/java/com/example/user/cloudplayer/MainActivity.java
+++ CloudPlayer/app/src/main/java/com/example/user/cloudplayer/MainActivity.java
@@ -84,7 +84,7 @@ public class MainActivity extends Activity implements NetworkEventListener {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(activity, PlayListActivity.class);
                 intent.putExtra(activity.getResources().getString(R.string.key_playlistID),
-                        playLists.get(i).getID());
+                        playLists.get(i));
                 startActivity(intent);
             }
         });
--- CloudPlayer/app/src/main/java/com/example/user/cloudplayer/model/PlayList.java
+++ CloudPlayer/app/src/main/java/com/example/user/cloudplayer/model/PlayList.java
@@ -1,8 +1,12 @@
 package com.example.user.cloudplayer.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
-public class PlayList {
+public class PlayList implements Parcelable{
+    private int mData;
     private String name;
     private String ID;
     private ArrayList<Song> songs;
@@ -46,5 +50,31 @@ public class PlayList {
 
     public String getUserID() {
         return userID;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /** save object in parcel */
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeInt(mData);
+    }
+
+    public static final Parcelable.Creator<PlayList> CREATOR
+            = new Parcelable.Creator<PlayList>() {
+        public PlayList createFromParcel(Parcel in) {
+            return new PlayList(in);
+        }
+
+        public PlayList[] newArray(int size) {
+            return new PlayList[size];
+        }
+    };
+
+    /** recreate object from parcel */
+    private PlayList(Parcel in) {
+        mData = in.readInt();
     }
 }
