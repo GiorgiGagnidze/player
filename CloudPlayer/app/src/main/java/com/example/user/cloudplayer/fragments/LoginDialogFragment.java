2015ba46abc48562db14f69adecdbfd244f3358d
GiorgiGagnidze <GiorgiGagnidze@users.noreply.github.com>
2015-07-12 18:48:59 +0400

    last but not least

--- CloudPlayer/app/src/main/java/com/example/user/cloudplayer/fragments/LoginDialogFragment.java
+++ CloudPlayer/app/src/main/java/com/example/user/cloudplayer/fragments/LoginDialogFragment.java
@@ -29,6 +29,7 @@ import java.util.regex.Pattern;
 public class LoginDialogFragment extends DialogFragment {
     private EditText editText;
     private EditText username;
+    private boolean checker;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -45,54 +46,63 @@ public class LoginDialogFragment extends DialogFragment {
         d.setContentView(R.layout.login_dialog);
         editText = (EditText)d.findViewById(R.id.edit_email);
         username = (EditText)d.findViewById(R.id.edit_username);
+        checker = true;
         if (savedInstanceState != null) {
             editText.setText(savedInstanceState.getString(getResources().getString(R.string.key_email)));
             username.setText(savedInstanceState.getString(getResources().getString(R.string.key_username)));
+            checker = savedInstanceState.getBoolean(getResources().getString(R.string.key_for_bool));
         }
         Button button = (Button)d.findViewById(R.id.sign_up);
-        final Activity activity=getActivity();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = editText.getText().toString();
-                String password = username.getText().toString();
-                if (password.equals("")){
-                    Toast.makeText(activity, activity.getResources().getString(R.string.user_name_alert),
-                            Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if (email.equals("")){
-                    Toast.makeText(activity, activity.getResources().getString(R.string.email_alert),
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    Pattern emailPattern = Patterns.EMAIL_ADDRESS;
-                    int counter = 0;
-                    Account[] accounts = AccountManager.get(activity.getApplicationContext()).getAccounts();
-                    for (Account account : accounts) {
-                        if (emailPattern.matcher(account.name).matches() &&
-                                email.equals(account.name)){
-                            handleEmail(email,d,activity,password);
-                            break;
-                        }
-                        counter++;
-                    }
-                    if (counter == accounts.length)
-                        Toast.makeText(activity, activity.getResources().getString(R.string.email_incorrect),
+                if (checker) {
+                    checker = false;
+                    String email = editText.getText().toString();
+                    String password = username.getText().toString();
+                    if (password.equals("")) {
+                        Toast.makeText(getActivity(), getActivity().getResources().getString(R.string.user_name_alert),
                                 Toast.LENGTH_LONG).show();
+                        checker = true;
+                        return;
+                    }
+                    if (email.equals("")) {
+                        Toast.makeText(getActivity(), getActivity().getResources().getString(R.string.email_alert),
+                                Toast.LENGTH_LONG).show();
+                        checker = true;
+                    } else {
+                        Pattern emailPattern = Patterns.EMAIL_ADDRESS;
+                        int counter = 0;
+                        Account[] accounts = AccountManager.get(getActivity().getApplicationContext()).getAccounts();
+                        for (Account account : accounts) {
+                            if (emailPattern.matcher(account.name).matches() &&
+                                    email.equals(account.name)) {
+                                handleEmail(email, d, password);
+                                break;
+                            }
+                            counter++;
+                        }
+                        if (counter == accounts.length) {
+                            Toast.makeText(getActivity(), getActivity().getResources().getString(R.string.email_incorrect),
+                                    Toast.LENGTH_LONG).show();
+                            checker = true;
+                        }
+                    }
                 }
             }
         });
         return d;
     }
 
-    private void handleEmail(final String email,final Dialog d,final Activity activity,final String
+    private void handleEmail(final String email,final Dialog d,final String
             password){
         ParseUser.logInInBackground(email, password, new LogInCallback() {
             @Override
             public void done(final ParseUser parseUser, com.parse.ParseException e) {
-                final SharedPreferences prefs = activity.getSharedPreferences(
+                final SharedPreferences prefs = getActivity().getSharedPreferences(
                         getResources().getString(R.string.key_app), Context.MODE_PRIVATE);
                 if (parseUser != null) {
+                    checker = true;
                     Log.i("ki", "daloginda");
                     prefs.edit().putString(getResources().getString(R.string.key_account), email).apply();
                     d.dismiss();
@@ -106,14 +116,16 @@ public class LoginDialogFragment extends DialogFragment {
                         @Override
                         public void done(ParseException e) {
                             if (e == null) {
+                                checker = true;
                                 Log.i("ki", "daregistrirda");
                                 prefs.edit().putString(getResources().getString(R.string.key_account)
                                         , email).apply();
                                 d.dismiss();
                             } else {
+                                checker = true;
                                 Log.i("ver", "daregistrirda");
-                                Toast.makeText(activity,
-                                        activity.getResources().getString(R.string.login_alert),
+                                Toast.makeText(getActivity(),
+                                        getActivity().getResources().getString(R.string.login_alert),
                                         Toast.LENGTH_LONG).show();
                             }
                         }
@@ -128,5 +140,6 @@ public class LoginDialogFragment extends DialogFragment {
         super.onSaveInstanceState(outState);
         outState.putString(getResources().getString(R.string.key_email),editText.getText().toString());
         outState.putString(getResources().getString(R.string.key_username),username.getText().toString());
+        outState.putBoolean(getResources().getString(R.string.key_for_bool),checker);
     }
 }
